2978d1483ae2095474b21b0afc5042f3304e31b5
Frede-72 <frjo0005@stud.kea.dk>
2025-09-03 13:03:01 +0200

    e

--- TuristGuide/src/main/java/com/example/turistguide/controller/TouristController.java
+++ TuristGuide/src/main/java/com/example/turistguide/controller/TouristController.java
@@ -2,14 +2,9 @@ package com.example.turistguide.controller;
 
 import com.example.turistguide.model.TouristAttraction;
 import com.example.turistguide.service.TouristService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/attractions")
@@ -25,48 +20,68 @@ public class TouristController {
         return (touristService.findAttractionByName(touristAttraction.getName()) != null);
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<TouristAttraction> addTouristAttraction(@RequestBody TouristAttraction touristAttraction) {
-        if (touristAttraction.getName() == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        if (!doesAttractionExist(touristAttraction)) {
-
-            return new ResponseEntity<>(touristService.addTouristAttraction(touristAttraction), HttpStatus.CREATED);
-
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
 
     @GetMapping()
     public String getAllTouristAttraction(Model model) {
-        model.addAttribute("attractions",touristService.getAllTouristAttraction());
-        return "showAttractions";
+        model.addAttribute("attractions", touristService.getAllTouristAttraction());
+        return "attractionList";
     }
 
     @GetMapping("{name}")
-    public ResponseEntity<TouristAttraction> getTouristAttraction(@PathVariable(required = false) String name) {
+    public String getTouristAttraction(@PathVariable(required = false) String name, Model model) {
         TouristAttraction touristAttraction = touristService.getTouristAttraction(name);
         if (touristAttraction != null) {
-            return new ResponseEntity<>(touristAttraction, HttpStatus.OK);
+            model.addAttribute("attraction", touristAttraction);
+            return "show-attraction";
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return "redirect:/attractions";
+    }
+
+    @GetMapping("{name}/tags")
+    public String showTagsForAttractions() {
+        return "";
+    }
+
+    @GetMapping("/add")
+    public String showTouristAttractionForm(Model model) {
+        TouristAttraction touristAttraction = new TouristAttraction();
+        model.addAttribute("attraction", touristAttraction);
+        return "add-Attraction-From";
+    }
+
+    @PostMapping("/save")
+    public String addTouristAttraction(@ModelAttribute TouristAttraction touristAttraction) {
+        TouristAttraction newAttraction = touristAttraction;
+        if (newAttraction.getName().isEmpty() || newAttraction.getDescription().isEmpty()) {
+            return "redirect:/attractions/add";
+        }
+        if (touristService.addTouristAttraction(newAttraction) == null) {
+            return "redirect:/attractions/add";
+        }
+        return "redirect:/attractions";
+    }
+
+    @GetMapping("/{name}/edit")
+    public String updateAttraction(@PathVariable String name, Model model) {
+        TouristAttraction touristAttraction = touristService.getTouristAttraction(name);
+        if (touristAttraction == null) {
+            return "redirect:/attractions";
+        }
+        model.addAttribute("attraction", touristAttraction);
+        return "update-attraction";
     }
 
     @PostMapping("/update")
-    public ResponseEntity<TouristAttraction> updateTouristAttraction(@RequestBody TouristAttraction touristAttraction) {
-        if (doesAttractionExist(touristAttraction)) {
-            return new ResponseEntity<>(touristService.updateTouristAttraction(touristAttraction), HttpStatus.ACCEPTED);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public String updateTouristAttraction(@ModelAttribute TouristAttraction touristAttraction) {
+        touristService.updateTouristAttraction(touristAttraction);
+        return "redirect:/attractions";
     }
 
     @PostMapping("/delete/{name}")
-    public ResponseEntity<TouristAttraction> deleteTouristAttraction(@PathVariable String name) {
-
-        TouristAttraction deletedTouristAttraction = touristService.findAttractionByName(name);
-        touristService.deleteTouristAttraction(name);
-        return new ResponseEntity<>(deletedTouristAttraction, HttpStatus.ACCEPTED);
+    public String deleteTouristAttraction(@ModelAttribute TouristAttraction touristAttraction) {
+        TouristAttraction deletedTouristAttraction = touristService.findAttractionByName(touristAttraction.getName());
+        touristService.deleteTouristAttraction(touristAttraction.getName());
+        return "redirect:/attractions";
     }
 
 }
--- TuristGuide/src/main/java/com/example/turistguide/service/TouristService.java
+++ TuristGuide/src/main/java/com/example/turistguide/service/TouristService.java
@@ -11,7 +11,7 @@ public class TouristService {
 
     private final TouristRepository touristRepository;
 
-    public TouristService(TouristRepository touristRepository){
+    public TouristService(TouristRepository touristRepository) {
         this.touristRepository = touristRepository;
     }
 
@@ -19,27 +19,30 @@ public class TouristService {
         return touristRepository.findAttractionByName(name);
     }
 
-    public TouristAttraction addTouristAttraction(TouristAttraction touristAttraction){
-        return touristRepository.addTouristAttraction(touristAttraction);
+    public TouristAttraction addTouristAttraction(TouristAttraction touristAttraction) {
+        if (touristRepository.findAttractionByName(touristAttraction.getName()) == null) {
+            return touristRepository.addTouristAttraction(touristAttraction);
+        }
+        return null;
     }
 
-    public List<TouristAttraction> getAllTouristAttraction(){
+    public List<TouristAttraction> getAllTouristAttraction() {
         return touristRepository.getAllTouristAttraction();
     }
 
-    public TouristAttraction getTouristAttraction(String name){
+    public TouristAttraction getTouristAttraction(String name) {
         return touristRepository.getTouristAttraction(name);
     }
 
-    public TouristAttraction updateTouristAttraction(TouristAttraction touristAttraction){
+    public TouristAttraction updateTouristAttraction(TouristAttraction touristAttraction) {
         return touristRepository.updateTouristAttraction(touristAttraction);
     }
 
-    public void deleteTouristAttraction(String name){
+    public void deleteTouristAttraction(String name) {
         touristRepository.deleteTouristAttraction(name);
     }
 
-    public boolean doesAttractionExist(TouristAttraction touristAttraction){
+    public boolean doesAttractionExist(TouristAttraction touristAttraction) {
         return findAttractionByName(touristAttraction.getName()) != null;
     }
 
